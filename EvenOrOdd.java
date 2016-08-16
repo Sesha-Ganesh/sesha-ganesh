e9de3c3e033d2201dea09e6ef66338c5d6150cf7
Sesha-Ganesh <seshaganeshs@gmail.com>
2016-08-16 18:19:55 +0530

    Create EvenOrOdd.java

---
+++ EvenOrOdd.java
@@ -0,0 +1,14 @@
+import.java.util.*;
+
+public class evenorodd
+{
+public static void main(String args[])
+{
+  Scanner sc=new Scanner(System.in);
+  int i=sc.nextInt();
+  if(i%2==0)
+    System.out.println("even number");
+  else
+    System.out.println("odd number");
+}
+}
