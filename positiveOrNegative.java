a52992c92b7464a471ada17638dd1e1a97ab1be4
Sesha-Ganesh <seshaganeshs@gmail.com>
2016-08-16 18:12:03 +0530

    Create positiveOrNegative.java

---
+++ positiveOrNegative.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class check
+{
+public static void main()
+{
+ Scanner sc=new Scanner(System.in);
+ int i=sc.nextInt();
+ if(i>0)
+ System.out.println("positive number");
+ else if(i<0)
+ System.out.println("negative number");
+ if(i==0)
+ System.out.println("zero");
+}
+}
