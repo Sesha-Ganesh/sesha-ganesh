27ea513cd533a38f5948bd92219edac28191a94f
Sesha-Ganesh <seshaganeshs@gmail.com>
2016-09-01 22:01:32 +0530

    Create strReversing.java

---
+++ strReversing.java
@@ -0,0 +1,9 @@
+public class strReversing
+{
+	public static void main(String[] args)
+	{
+	  Scanner sc=new Scanner(System.in);
+  	String g=sc.nextLine();
+  	System.out.println(new StringBuffer(g).reverse());
+	}
+}
